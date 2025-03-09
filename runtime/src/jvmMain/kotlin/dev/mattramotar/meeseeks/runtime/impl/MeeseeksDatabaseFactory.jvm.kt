45bd9e36cf38807d1f40d4aae08a943d3cecf7ca
Matt Ramotar <matt.ramotar@uber.com>
2025-03-09 10:14:35 -0400

    Add jvmMain implementation
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>

--- runtime/src/jvmMain/kotlin/dev/mattramotar/meeseeks/runtime/impl/MeeseeksDatabaseFactory.jvm.kt
+++ runtime/src/jvmMain/kotlin/dev/mattramotar/meeseeks/runtime/impl/MeeseeksDatabaseFactory.jvm.kt
@@ -5,6 +5,7 @@ import dev.mattramotar.meeseeks.runtime.MeeseeksContext
 import dev.mattramotar.meeseeks.runtime.db.MeeseeksDatabase
 import dev.mattramotar.meeseeks.runtime.db.TaskEntity
 import dev.mattramotar.meeseeks.runtime.db.TaskLogEntity
+import java.sql.SQLException
 
 internal actual class MeeseeksDatabaseFactory actual constructor() {
     actual fun create(
@@ -13,6 +14,24 @@ internal actual class MeeseeksDatabaseFactory actual constructor() {
         taskLogAdapter: TaskLogEntity.Adapter
     ): MeeseeksDatabase {
         val driver = JdbcSqliteDriver("jdbc:sqlite:meeseeks.db", schema = MeeseeksDatabase.Schema)
-        return MeeseeksDatabase(driver, taskAdapter, taskLogAdapter)
+        val database = MeeseeksDatabase(driver, taskAdapter, taskLogAdapter)
+        runQuartzSchema(driver)
+        return database
+    }
+
+    private fun runQuartzSchema(driver: JdbcSqliteDriver) {
+        val resourcePath = "/dev/mattramotar/meeseeks/runtime/impl/quartz/tables_sqlite.sql"
+        val stream = this::class.java.getResourceAsStream(resourcePath)
+            ?: throw IllegalStateException("Could not find $resourcePath")
+
+        val script = stream.bufferedReader().readText()
+        val statements = script.split(";").map { it.trim() }.filter { it.isNotEmpty() }
+
+        for (sql in statements) {
+            try {
+                driver.execute(null, sql, 0)
+            } catch (_: SQLException) {
+            }
+        }
     }
 }
--- runtime/src/jvmMain/kotlin/dev/mattramotar/meeseeks/runtime/impl/WorkRequest.jvm.kt
+++ runtime/src/jvmMain/kotlin/dev/mattramotar/meeseeks/runtime/impl/WorkRequest.jvm.kt
@@ -1,5 +1,13 @@
 package dev.mattramotar.meeseeks.runtime.impl
 
+import org.quartz.JobDetail
+import org.quartz.Trigger
+
+
 internal actual class WorkRequest(
+    val jobDetail: JobDetail,
+    val triggers: List<Trigger>
+) {
     actual val id: String
-)
+        get() = jobDetail.key.name
+}
